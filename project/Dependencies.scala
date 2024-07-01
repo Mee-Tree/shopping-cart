84532c6650508d2ae13e1699ee011b161251ea2a
Mee-Tree <meetree@ya.ru>
2024-07-01 01:13:36 +0000

    Update circe-core, circe-generic, ... to 0.14.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val cats              = "2.12.0"
     val `cats-effect`     = "3.5.4"
     val `cats-retry`      = "3.1.3"
-    val circe             = "0.14.7"
+    val circe             = "0.14.8"
     val ciris             = "2.3.3"
     val derevo            = "0.13.0"
     val fs2               = "3.10.2"
